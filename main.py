6de77ee3d7b14d3e72d0c733928ff42c725fe1a6
RenattoNL <renattonl@gmail.com>
2021-02-11 17:03:27 -0500

    Update mesearument and mesuarementlist

--- main.py
+++ main.py
@@ -1,74 +1,109 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, reqparse
 import uuid
 import json
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date
+from flask_mongoengine import MongoEngine
+from mongoengine import Document, EmailField, StringField, IntField, DateTimeField, ReferenceField
+from mongoengine.errors import ValidationError, NotUniqueError
+from werkzeug.exceptions import NotFound, BadRequest
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine()
+
 
 API_HOST = os.environ.get("API_HOST", "localhost")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_NAME = os.environ.get("DB_NAME", "")
+DB_USER = os.environ.get("DB_USER", "")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+
+app.config["MONGODB_SETTINGS"] = {
+    "host": DB_HOST,
+    "db": DB_NAME,
+    "connect": False,
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "authentication_source": "admin"
+}
+db.init_app(app)
 
 
-def generate_uuid():
-    identifier = uuid.uuid4()
-    return json.dumps(identifier, default=str)
+class User(Document):
+    email = EmailField(required=True)
+    name = StringField(max_length=255, required=True)
+    surname = StringField(max_length=255, required=True)
 
 
-def get_utc_now():
-    now = datetime.utcnow().replace(tzinfo=timezone.utc)
-    return json.dumps(now, default=str)
+class Measurement(Document):
+    sys = IntField(min_value=0, max_value=200, required=True)
+    dia = IntField(min_value=0, max_value=200, required=True)
+    pul = IntField(min_value=0, max_value=200, required=True)
+    created = DateTimeField(default=date.today, unique=True)
+    user = ReferenceField(User, required=True)
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "sys": self.sys,
+            "dia": self.dia,
+            "pul": self.pul,
+            "created": self.created.strftime("%Y-%m-%d"),
+            "user": str(self.user.id),
+        }
 
 
-MEASUREMENTS = [
-    {
-        "id": "654646546546",
-        "sys": 120,
-        "dia": 80,
-        "pul": 70,
-        "created": get_utc_now(),
-        "user_id": "65465465465465465"
-    },
-    {
-        "id": "7574635453453",
-        "sys": 170,
-        "dia": 90,
-        "pul": 70,
-        "created": get_utc_now(),
-        "user_id": "2345647656768"
-    },
-]
-
-
-class Measurement(Resource):
+class MeasurementDetail(Resource):
     def get(self, id):
-
-        for measurement in MEASUREMENTS:
-            if id == measurement.get('id'):
-                return measurement, 200
-        abort(404, message=f'Measurement ID={id} was not found')
+        try:
+            measurement = Measurement.objects(id=id).first()
+            if measurement is not None:
+                return measurement.to_dict(), 200
+            abort(404, message=f'Measurement ID={id} was not found')
+        except NotFound as e:
+            raise e
+        except Exception as e:
+            abort(500, message=str(e))
 
 
 class MeasurementList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            "sys", type=int, required=True, location="json")
+        self.reqparse.add_argument(
+            "dia", type=int, required=True, location="json")
+        self.reqparse.add_argument(
+            "pul", type=int, required=True, location="json")
+        super(MeasurementList, self).__init__()
+
     def get(self):
-        return MEASUREMENTS, 200
+        try:
+            data = [measurement.to_dict()
+                    for mesearement in Measurement.objects]
+            return data, 200
+        except Exception as e:
+            abort(500, message=str(e))
 
     def post(self):
-        data = json.loads(request.data)
-        measurement = {
-            "id": generate_uuid(),
-            "sys": data.get("sys"),
-            "dia": data.get("dia"),
-            "pul": data.get("pul"),
-            "created": get_utc_now(),
-            "user_id": "2345647656768"
-        }
-        MEASUREMENTS.append(measurement)
-        return measurement, 201
+        try:
+            data = self.reqparse.parse_args()
+            measurement = Measurement(**data)
+            user = User.objects(email="renattonl@gmail.com").first()
+            measurement.user = user
+            measurement.save()
+            return measurement.to_dict(), 201
+        except BadRequest as e:
+            abort(400, message=e.data.get("message"))
+        except (NotUniqueError, ValidationError) as e:
+            abort(400, message=str(e))
+        except Exception as e:
+            abort(500, message=str(e))
 
 
-api.add_resource(Measurement, "/v1/measurements/<string:id>")
+api.add_resource(MeasurementDetail, "/v1/measurements/<string:id>")
 api.add_resource(MeasurementList, "/v1/measurements")
 
 if __name__ == "__main__":
